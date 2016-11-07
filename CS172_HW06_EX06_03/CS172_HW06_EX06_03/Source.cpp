76a458cc235d5cd5033321e4c540bdebae89613b
ifisher18 <ifisher18@LAB-SCI-214-21>
2016-11-06 20:10:36 -0800

    more changes-- need some help here

--- CS172_HW06_EX06_03/CS172_HW06_EX06_03/Source.cpp
+++ CS172_HW06_EX06_03/CS172_HW06_EX06_03/Source.cpp
@@ -5,6 +5,9 @@
 ///////////////////
 
 #include <iostream>
+#include <string>
+#include <fstream>
+
 using namespace std;
 
 int main() {
@@ -12,6 +15,7 @@ int main() {
 	char gender;
 	string name;
 	// Prompts the user to enter year, gender, followed by a name
+	// year, gender, and name are entered
 	cout << "Enter the year: ";
 	cin >> year;
 
@@ -20,6 +24,40 @@ int main() {
 
 	cout << "Enter the name: ";
 	cin >> name;
+
+	// files are opened
+	ofstream fout2010;
+	ofstream fout2011;
+	ofstream fout2012;
+	ofstream fout2013;
+	ofstream fout2014;
+	fout2010.open("Babynameranking2010.txt");
+	if (fout2010.fail()) {
+		cout << "can't open Babynameranking2010.txt file" << endl;
+	}
+
+	fout2011.open("Babynameranking2011.txt");
+	if (fout2011.fail()) {
+		cout << "can't open Babynameranking2011.txt file" << endl;
+	}
+
+	fout2012.open("Babynameranking2012.txt");
+	if (fout2012.fail()) {
+		cout << "can't open Babynameranking2012.txt file" << endl;
+	}
+
+	fout2013.open("Babynameranking2013.txt");
+	if (fout2013.fail()) {
+		cout << "can't open Babynameranking2013.txt file" << endl;
+	}
+
+	fout2014.open("Babynameranking2014.txt");
+	if (fout2014.fail()) {
+		cout << "can't open Babynameranking2014.txt file" << endl;
+	}
+	//Test
+	cout << "Files correctly opened" << endl;
+
 
 	// displays the ranking of the name for the year
 }
