032ca0f0dfab5386863734d812a7f4b89839ff91
ifisher18 <ifisher18@LAB-SCI-214-21>
2016-11-06 20:00:11 -0800

    Initial Commit-- changes

---
+++ CS172_HW06_EX06_03/CS172_HW06_EX06_03/Source.cpp
@@ -0,0 +1,25 @@
+////////////////////
+// Ian Fisher
+// CS 172
+// 11/6/16
+///////////////////
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int year;
+	char gender;
+	string name;
+	// Prompts the user to enter year, gender, followed by a name
+	cout << "Enter the year: ";
+	cin >> year;
+
+	cout << "Enter the gender: ";
+	cin >> gender;
+
+	cout << "Enter the name: ";
+	cin >> name;
+
+	// displays the ranking of the name for the year
+}
