c6bdc473370a2b2681e0e0cd173c0ada21479280
ifisher18 <ifisher18@LAB-SCI-214-21>
2016-11-06 19:41:15 -0800

    Initial Commit-- changes

---
+++ CS172_HW06_EX06_02/CS172_HW06_EX06_02/Source.cpp
@@ -0,0 +1,47 @@
+////////////////////
+// Ian Fisher
+// CS 172
+// 11/6/16
+////////////////////
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+	string input = "";
+	// prompts the user to enter a file name
+	string FileName;
+	cout << "Hello! Please enter a file name: ";
+	getline(cin, input);
+
+	//
+	int count = 0;
+	fstream iofile(FileName, ios::in | ios::out); // opens file, etc.
+	if (!iofile) {
+		cout << "Error: can't open file" << endl;
+	}
+
+	else
+	{
+		string character;
+		while (iofile >> character) {
+			count++;
+		}
+		iofile.clear();
+		iofile.seekg(0, ios::end);
+		// Displays the number characters in the file
+		iofile << "\n\nThis file has " << count << " characters." << endl;
+	}
+
+
+
+
+
+	
+
+	
+}
