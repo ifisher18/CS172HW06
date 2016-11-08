74e383508fb8c8e2c6a89102f283cf3aee4a653f
ifisher18 <ifisher18@LAB-SCI-214-16>
2016-11-07 19:50:37 -0800

    Getting there....

--- CS172_HW06_EX06_04/CS172_HW06_EX06_04/Source1.cpp
+++ CS172_HW06_EX06_04/CS172_HW06_EX06_04/Source1.cpp
@@ -8,12 +8,57 @@
 #include "Circle.h"
 using namespace std;
 
+// I interpretted the end of this problem to mean to write a test of some of the operator overload functions in class Circle:
+// Testing
 int main() {
 	Circle circle1; // create object circle1
 	Circle circle2(6.0); // create object circle2 with radius 6
+	Circle circle3(10.0); // create object circle3 with radius 10.0
+	Circle circle4; // creates object circle4
+	Circle circle5(10.0); // create object circle5 of radius 10
 
+	// cout what is being tested
+	cout << "Testing:" << endl;
+	cout << "Circle circle1" << endl;
+	cout << "Circle circle2(6.0)" << endl;
+	cout << "Circle circle3(10.0)" << endl;
 
+	// Testing < operator function
+	if (circle1 < circle2) {
+		cout << "circle2 is larger than circle1" << endl;
+		cout << "The area of the circle of radius " << circle1.getRadius() << " is "
+			<< circle1.getArea() << endl;
+		cout << "The area of the circle of radius " << circle2.getRadius() << " is "
+			<< circle2.getArea() << endl << endl;
+	}
+	else cout << "circle2 is smaller than circle1" << endl;
+	cout << endl;
 
-	// what does it mean to order the Circle objects according to their radii???????
+	// Testing <= operator function
+	if (circle1 <= circle4) {
+		cout << "circle1 is less than or equal to circle4" << endl;
+		cout << "The area of the circle of radius " << circle1.getRadius() << " is "
+			<< circle1.getArea() << endl;
+		cout << "The area of the circle of radius " << circle2.getRadius() << " is "
+			<< circle2.getArea() << endl << endl;
+	}
+	else cout << "circle1 is greater than circle4" << endl;
+	cout << endl;
+
+	// Testing == operator function
+	if (circle3 == circle4) {
+		cout << "circle3 is equal to circle4" << endl;
+		cout << "The area of the circle of radius " << circle1.getRadius() << " is "
+			<< circle1.getArea() << endl;
+		cout << "The area of the circle of radius " << circle2.getRadius() << " is "
+			<< circle2.getArea() << endl << endl;
+	}
+	else cout << "circle3 is not equal to circle4" << endl;
+	cout << endl;
+
+	// Testing == operator function
+	if
+
+	
 
 }
